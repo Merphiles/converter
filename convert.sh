ef96b08d42e042977cf83002e2df2fb8113bdd07
Merphiles <alisherz483@gmail.com>
2021-07-30 17:09:04 +0600

    Updated conver.sh

--- convert.sh
+++ convert.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 set -e
 FILES=$(find ./ -name '*video.mjr')
+FILESOPUS=$(find ./ -name '*audio.mjr')
 set +e
 for video in $FILES
 do
-  printf "\033[36mProcessing\033[0m $filename\n"
-  printf "  -> \033[35mExtracting video\033[0m\n"
-  janus-pp-rec $video $video.webm >/dev/null 2>&1
   filename=$(echo $video | awk -F"-" '{print $1"-"$2"-"$3"-"$4"-"$5}')
-  printf "  -> \033[35mExtracting audio\033[0m\n"
-  janus-pp-rec $filename"-audio.mjr" $filename"-audio".opus >/dev/null 2>&1
+  echo $video
+  janus-pp-rec $video $filename"-video".webm >/dev/null 2>&1
+for audio in $FILESOPUS
+do
+  echo $audio
+  filename=$(echo $audio | awk -F"-" '{print $1"-"$2"-"$3"-"$4"-"$5}')
+  janus-pp-rec $audio $filename"-audio".opus >/dev/null 2>&1
+done
 done
