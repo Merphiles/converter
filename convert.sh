2cc3956733646b931877ba4152228f7ecdfd48b9
Merphiles <alisherz483@gmail.com>
2021-08-02 12:45:21 +0600

    Updated shell script

--- convert.sh
+++ convert.sh
@@ -5,12 +5,13 @@ FILESOPUS=$(find ./ -name '*audio.mjr')
 set +e
 for video in $FILES
 do
+  printf "\033[36mProcessing\033[0m $filename\n"
+  printf "  -> \033[35mExtracting video\033[0m\n"
   filename=$(echo $video | awk -F"-" '{print $1"-"$2"-"$3"-"$4"-"$5}')
-  echo $video
   janus-pp-rec $video $filename"-video".webm >/dev/null 2>&1
 for audio in $FILESOPUS
 do
-  echo $audio
+  printf "  -> \033[35mExtracting audio\033[0m\n"
   filename=$(echo $audio | awk -F"-" '{print $1"-"$2"-"$3"-"$4"-"$5}')
   janus-pp-rec $audio $filename"-audio".opus >/dev/null 2>&1
 done
